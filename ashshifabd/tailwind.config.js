48c9107128a50e87318bfd04ebcb32152ffd1b31
md-parves <parves.dev@gmail.com>
2024-10-07 17:35:46 +0600

    add ashshifabd

--- ashshifabd/tailwind.config.js
+++ ashshifabd/tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       backgroundImage: {
         "header-gradient": "linear-gradient(94deg, #0AAF01 39%, #82BD3B 100%)",
       },
+      backgroundImageButton: {
+        "custom-gradient": "linear-gradient(158deg, #09B300 0%, #87C33F 100%)",
+      },
     },
   },
   plugins: [],
